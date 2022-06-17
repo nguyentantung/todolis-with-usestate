58a894c3611ca69509a19d47537762f23cc273ef
Tung <nguyentantung@gmail.com>
2022-06-17 08:58:29 +0700

    d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState([]);
+  const handleSetJob = (e) => {
+    setJob(e.target.value);
+  };
+  const handleAddJobs = () => {
+    setJobs([...jobs, job]);
+    setJob("");
+  };
+  const deleteJob = (index) => {
+    const newJobs = [...jobs];
+    newJobs.splice(index, 1);
+    setJobs(newJobs);
+  };
+  console.log(jobs);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="toDOList">
+        <div className="setTask">
+          <input value={job} onChange={handleSetJob} placeholder="Nhập task" />
+          <button onClick={handleAddJobs}>Nhập</button>
+        </div>
+        <div className="listTasks">
+          <ul>
+            {jobs.map((value, index) => {
+              return (
+                <li key={index}>
+                  <span>{value}</span>
+                  <span
+                    onClick={() => deleteJob(index)}
+                    style={{ cursor: "pointer", paddingLeft: "10px" }}
+                  >
+                    x
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
